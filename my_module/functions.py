954ea2d76b330e2f804deadb981f8399f1194f94
Rohan Kaman <rohankaman@gmail.com>
2023-12-09 13:13:23 -0800

    removing debugging case

--- my_module/functions.py
+++ my_module/functions.py
@@ -34,7 +34,6 @@ def fetch_real_time_flights(api_key):
     params = {'access_key': api_key}
     api_result = requests.get('http://api.aviationstack.com/v1/flights', params)
     api_response = api_result.json()
-    print(api_response)
 
     flights_in_air = []
 
