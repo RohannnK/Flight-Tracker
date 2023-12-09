32bd5d8a3ef16eca90a9d42cf90e52573686fd3a
Rohan Kaman <rohankaman@gmail.com>
2023-12-09 13:43:39 -0800

    made modifications to functions.py
    tried to fix error of empty list

--- my_module/functions.py
+++ my_module/functions.py
@@ -1,5 +1,5 @@
 import requests
-from .classes import Flight  # Import the Flight class from your classes module
+from .classes import Flight  # Import the Flight class from classes module
 
 def fetch_real_time_flights(api_key):
     """
@@ -31,12 +31,17 @@ def fetch_real_time_flights(api_key):
     >>> for flight in flights:
     ...     print(flight)
     """
+    # Parameters for the API request
     params = {'access_key': api_key}
+    
+    # Making the API request
     api_result = requests.get('http://api.aviationstack.com/v1/flights', params)
     api_response = api_result.json()
 
+    # List to hold flight information
     flights_in_air = []
 
+    # Processing each flight in the response
     for flight in api_response.get('results', []):
         if not flight['live']['is_ground']:
             flight_info = {
@@ -55,6 +60,9 @@ def process_flight_data(flight_data):
     """
     Processes raw flight data from the AviationStack API.
 
+    This function takes a list of flight data dictionaries and converts
+    them into Flight objects.
+
     Parameters
     ----------
     flight_data : list of dict
@@ -65,21 +73,23 @@ def process_flight_data(flight_data):
     list of Flight
         A list of Flight objects with processed data.
     """
-
+    # List to hold processed Flight objects
     processed_flights = []
 
+    # Processing each flight in the data
     for flight in flight_data:
-        # Extract relevant information
-        flight_number = flight.get('flight_number')
-        airline = flight.get('airline')
-        departure_airport = flight.get('departure')
-        departure_code = flight.get('departure_code')
-        arrival_airport = flight.get('arrival')
-        arrival_code = flight.get('arrival_code')
+        # Extracting relevant information
+        flight_number = flight['flight_number']
+        airline = flight['airline']
+        departure_airport = flight['departure']
+        departure_code = flight['departure_code']
+        arrival_airport = flight['arrival']
+        arrival_code = flight['arrival_code']
 
-        # Create a Flight object
+        # Creating a Flight object
         processed_flight = Flight(flight_number, airline, departure_airport, departure_code, arrival_airport, arrival_code)
         
+        # Adding the Flight object to the list
         processed_flights.append(processed_flight)
 
     return processed_flights
