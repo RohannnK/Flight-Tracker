ebcd6dc6d481ddc9c0853720a18229332e948e9c
Rohan Kaman <rohankaman@gmail.com>
2023-12-09 13:51:28 -0800

    debugging flight processing

--- my_module/functions.py
+++ my_module/functions.py
@@ -58,10 +58,13 @@ def fetch_real_time_flights(api_key):
 
 def process_flight_data(flight_data):
     """
-    Processes raw flight data from the AviationStack API.
+    Processes raw flight data into Flight objects.
 
-    This function takes a list of flight data dictionaries and converts
-    them into Flight objects.
+    This function takes a list of flight data dictionaries, each representing
+    a flight's data, and converts them into Flight objects. It ensures that
+    each flight record has all the necessary information before creating a
+    Flight object. If any information is missing, it skips that record and
+    prints a message.
 
     Parameters
     ----------
@@ -72,24 +75,37 @@ def process_flight_data(flight_data):
     -------
     list of Flight
         A list of Flight objects with processed data.
+
+    Notes
+    -----
+    The function uses a try-except block to catch any unexpected errors during
+    the processing of each flight record.
     """
+
     # List to hold processed Flight objects
     processed_flights = []
 
     # Processing each flight in the data
     for flight in flight_data:
-        # Extracting relevant information
-        flight_number = flight['flight_number']
-        airline = flight['airline']
-        departure_airport = flight['departure']
-        departure_code = flight['departure_code']
-        arrival_airport = flight['arrival']
-        arrival_code = flight['arrival_code']
+        try:
+            # Safely extracting flight details
+            flight_number = flight.get('flight_number')
+            airline = flight.get('airline')
+            departure_airport = flight.get('departure')
+            departure_code = flight.get('departure_code')
+            arrival_airport = flight.get('arrival')
+            arrival_code = flight.get('arrival_code')
 
-        # Creating a Flight object
-        processed_flight = Flight(flight_number, airline, departure_airport, departure_code, arrival_airport, arrival_code)
-        
-        # Adding the Flight object to the list
-        processed_flights.append(processed_flight)
+            # Check if all required data is present
+            if all([flight_number, airline, departure_airport, departure_code, arrival_airport, arrival_code]):
+                # Creating a Flight object
+                processed_flight = Flight(flight_number, airline, departure_airport, departure_code, arrival_airport, arrival_code)
+                processed_flights.append(processed_flight)
+            else:
+                # Print a message if any data is missing
+                print("Missing data in flight record:", flight)
+        except Exception as e:
+            # Print any errors encountered during processing
+            print("Error processing flight data:", e)
 
     return processed_flights
